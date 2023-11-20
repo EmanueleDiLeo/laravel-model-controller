430795fa97d9f26052783c95c35212ffd39a77ea
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-11-20 17:36:34 +0100

    [ADD] : card movie

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <div class="container">
+
+        <h1 class="text-center ">MOVIES</h1>
+        <div class="row mt-5">
+
+            @foreach ($movies as $movie)
+                <div class="col-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                        <h4 class="card-title">Titolo: {{$movie->title}}</h4>
+                        <h5 class="card-title">Titolo Originale: {{$movie->original_title}}</h5>
+                        <p class="card-text">Nazione: {{$movie->nationality}}</p>
+                        <p class="card-text">Data di uscita: {{$movie->date}}</p>
+                        <p class="card-text">Voto: {{$movie->vote}}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+             @endforeach
+
+        </div>
+    </div>
+
+@endsection
+
+@section('title')
+    | Homepage
+@endsection
